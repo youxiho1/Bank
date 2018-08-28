5136c0980f3618c07df65686e42d0724448d4407
alex <330953853@qq.com>
2018-08-28 15:40:06 +0800

    用户类完善

--- MFCBANK/user.cpp
+++ MFCBANK/user.cpp
@@ -11,34 +11,20 @@ using namespace std;
 const char* dbFile = "bank.db";     //数据库名
 CppSQLite3DB db;                    //数据库类实例
 
+//初始化user
 User::User():account(0),name(""),hash_password(""),openDate(""),
 			isLost(0),lostDate(""),address(""),idNumber(""),salt("")
 			{
 }
 
+//生成密码
 string User::make_password(string password){
 	salt = rand_str();
 	string newPassword = salt + password + salt;
-	hash_password = picosha2::hash256_hex_string(newPassword.begin(), newPassword.end());
-	return hash_password;
+	return picosha2::hash256_hex_string(newPassword.begin(), newPassword.end());
 }
 
-bool User::login(int account, string password){
-	db.open(dbFile);
-	string sql ="select * from user where account = " + to_string(account);
-    CppSQLite3Query q = db.execQuery(sql.c_str());
-    for (int i = 0; i < q.numFields(); ++i) {
-        if (q.fieldValue(i)!= NULL) {
-            cout << q.fieldName(i) << ": "  << q.fieldValue(i) << endl;
-        }else{
-            cout << q.fieldName(i) << ": "  << "NULL" << endl;
-        }
-        
-    }
-    db.close();
-    return false;
-}
-
+//今天开户的用户数量
 int num_create_account_of_today(){
     db.open(dbFile);
     string sql ="select * from user where opendate = \"" + nowTime_to_str() + "\"" ;
@@ -47,6 +33,7 @@ int num_create_account_of_today(){
     return t.numRows();
 }
 
+//创建新账号
 int newAccount(){
     int number = num_create_account_of_today();
     time_t now = time(0);
@@ -69,6 +56,30 @@ int newAccount(){
     return atoi((to_string(year) + str_mon + str_day + to_string(number)).c_str());
 }
 
+//保存到数据库
+bool User::save(){
+    db.open(dbFile);
+    string searchSql = "select * from user where account = " + to_string(account);
+    CppSQLite3Table t = db.getTable(searchSql.c_str());
+    if (t.numRows() > 0) {
+        string sql = "update user set name =  \"" + name + "\",isLost=" + to_string(isLost) + ",";
+        sql += "lostDate=\"" + lostDate + "\",address = \"" + address + "\",idnumber = \"";
+        sql += idNumber + "\"," + "password = \"" + hash_password + "\"" + ",salt=\"" + salt;
+        sql += "\" where account = " + to_string(account);
+       
+        db.execDML(sql.c_str());
+    }
+    else{
+        string sql = "insert into user(account,name,password,opendate,islost,lostdate,address,idnumber,salt)";
+        sql += " values(" + to_string(account) + ",\"" + name + "\",\"" + hash_password + "\",\"" + openDate + "\"," + to_string(isLost);
+        sql += ",\"" + lostDate + "\",\"" + address + "\",\"" + idNumber + "\",\"" + salt + "\");";
+        db.execDML(sql.c_str());
+    }
+    db.close();
+    return true;
+}
+
+//创建用户
 bool User::create_account(string newName, string password,string newAddress,string newIdNumber){
     name = newName;
     hash_password = make_password(password);
@@ -76,15 +87,61 @@ bool User::create_account(string newName, string password,string newAddress,stri
     idNumber = newIdNumber;
     openDate = nowTime_to_str();
     account = newAccount();
+    
+    save();
     return true;
 }
 
-bool User::save(){
+//设置挂失
+void User::set_Lost(){
+    isLost = 1;
+    lostDate = nowTime_to_str();
+    save();
+}
+
+//用户登录
+bool User::login(int account, string password){
     db.open(dbFile);
-    string sql = "insert into user(account,name,password,opendate,islost,lostdate,address,idnumber,salt)";
-    sql += " values(" + to_string(account) + ",\"" + name + "\",\"" + hash_password + "\",\"" + openDate + "\"," + to_string(isLost);
-    sql += ",\"" + lostDate + "\",\"" + address + "\",\"" + idNumber + "\",\"" + salt + "\");";
-    db.execDML(sql.c_str());
+    string sql ="select * from user where account = " + to_string(account);
+    CppSQLite3Table t = db.getTable(sql.c_str());
     db.close();
-    return true;
+    if(t.numRows() == 1){
+        hash_password = t.fieldValue(3);
+        salt = t.fieldValue(9);
+        string userPassword = salt + password + salt;
+        userPassword = picosha2::hash256_hex_string(userPassword.begin(),userPassword.end());
+        if (userPassword == hash_password) {
+            this->account = account;
+            this->name = t.fieldValue(2);
+            this->openDate = t.fieldValue(4);
+            this->isLost = atoi(t.fieldValue(5)) ;
+            this->lostDate = t.fieldValue(6);
+            this->address = t.fieldValue(7);
+            this->idNumber = t.fieldValue(8);
+            return true;
+        }
+        return false;
+    }else{
+        return false;
+    }
+}
+
+//修改密码
+void User::change_password(string newPassword){
+    hash_password = make_password(newPassword);
+    save();
+}
+
+//设置用户信息
+void User::set_name(string newName){
+    name = newName;
+    save();
+}
+void User::set_address(string newAddress){
+    address = newAddress;
+    save();
+}
+void User::set_idNumber(string newIdNumber) {
+    idNumber = newIdNumber;
+    save();
 }
--- MFCBANK/user.h
+++ MFCBANK/user.h
@@ -17,22 +17,22 @@ public:
   	int get_account(){ return account; }
   	string get_name(){ return name; }
   	string get_openDate(){ return openDate; }
-  	bool get_isLost(){ return isLost; }
+  	int  check_Lost(){ return isLost; }
   	string get_lostDate(){ return lostDate; }
   	string get_address(){ return address; }
   	string get_idNumber(){ return idNumber; }
 
   	//设置用户信息
-  	void set_name(string newName){ name = newName; }
-  	void set_address(string newAddress){ address = newAddress; }
-  	void set_idNumber(string newIdNumber) { idNumber = newIdNumber; }
+    void set_name(string newName);
+    void set_address(string newAddress);
+    void set_idNumber(string newIdNumber);
   	
-  	string make_password(string);	//生成密码
-  	bool login(int, string);		//判断用户账号与密码，正确则初始化用户变量
-  	bool create_account(string,string,string,string);			//创建用户
-  	bool save();					//存入数据库
-    bool update();                  //更新用户信息
-    void set_isLost();              //挂失
+  	string make_password(string);	                    //生成密码
+  	bool login(int, string);		                    //判断用户账号与密码，正确则初始化用户变量
+  	bool create_account(string,string,string,string);	//创建用户
+  	bool save();					                    //存入数据库
+    void set_Lost();                                    //挂失
+    void change_password(string);                       //修改密码
     
 private:
 	int account;
