f8c64e2e00383547ba6799db808c2276b28f70c5
alex <330953853@qq.com>
2018-08-28 08:49:38 +0800

    增加随机字符串

---
+++ MFCBANK/rand_string.h
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <ctime>
+#include <string>
+
+const int SIZE_CHAR = 11; //生成9 + 1位C Style字符串
+const char CCH[] = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+std::string rand_str()
+{	
+	srand((unsigned)time(0));
+   	char ch[SIZE_CHAR + 1] = {0};
+   
+   	for (int i = 0; i < SIZE_CHAR; ++i)
+   	{
+   		int x = rand() / (RAND_MAX / (sizeof(CCH) - 1));
+   		ch[i] = CCH[x];
+   	}
+	
+	for (int i = 0; i < SIZE_CHAR; ++i)
+	{
+		ch[i] = ch[i+1];  	
+	}     
+    return ch;
+}
